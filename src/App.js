295c0b996f2c51927b225a12cd18e9b5cbc47082
Nacib Mariem <mariem.nacib@esprit.tn>
2023-08-20 18:10:28 +0100

    my first commit pre deploy

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import me from './assets/img/me.jpg';
+function App() {
+  return (
+    <div className="App">
+      <h1>this is my portfolio </h1>
+      <h2>My name is : </h2><h1> NACIB MARIEM</h1>
+      <img src={me} alt="this is me"></img>
+    </div>
+  );
+}
+
+export default App;
